595508f5921ab1065597dd738707a36c675d182a
Robert Kirkum <robertkirkum5979@hotmail.com>
2018-04-07 23:15:26 -0500

    added bootstrap, jquery, knockout, trackingQuestion contorller and home viewModel

---
+++ src/main/java/com/example/teamrocket/controllers/TrackingQuestionController.java
@@ -0,0 +1,32 @@
+package com.example.teamrocket.controllers;
+
+import com.example.teamrocket.data.HealthTraitDao;
+import com.example.teamrocket.data.TrackingQuestionDao;
+import com.example.teamrocket.models.*;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Tracking-Question")
+public class TrackingQuestionController {
+
+	@Autowired
+    private TrackingQuestionDao trackingQuestionDao;
+	
+
+    @RequestMapping(value = "getTrackingQuestion", method = RequestMethod.GET)
+    public TrackingQuestion getATrackingQuestion() {
+    	
+        TrackingQuestion trackingQuestion = trackingQuestionDao.findOne(1);
+        return trackingQuestion;
+    }
+}
